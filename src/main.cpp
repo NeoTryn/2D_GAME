ee3cab3bb4c6c02e8edaa54579ae0e77b7e6d63d
NeoTryn <felixeidler@gmail.com>
2025-11-06 12:50:16 +0100

    Added basic shaders

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,28 @@
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
+const char* vertexShaderSrc = 
+"#version 460 core\n"
+"layout (location = 0) in vec3 vertPos;\n"
+"\n"
+"void main() {\n"
+"	gl_Position = vec4(vertPos, 1.0f);\n"
+"}\n";
+
+const char* fragmentShaderSrc = 
+"#version 460 core\n"
+"out vec4 fragCol;\n"
+"\n"
+"void main() {\n"
+"	fragCol = vec4(0.9f, 0.3f, 0.5f, 1.0f);\n"
+"}\n";
+
+const float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f
+};
+
 int main() {
 
 	glfwWindowHint(GLFW_VERSION_MAJOR, 4);
@@ -39,6 +61,33 @@ int main() {
 
 	glClearColor(0.3f, 0.5f, 0.6f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	unsigned int vertexShader, fragmentShader;
+
+	char infoLog[512];
+	int success;
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSrc, nullptr);
+	glCompileShader(vertexShader);
+	
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+		std::cout << "Vertex Shader compilation failed. Infolog: " << infoLog << std::endl;
+	}
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSrc, nullptr);
+	glCompileShader(fragmentShader);
+		
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+		std::cout << "Fragment Shader compilation failed. Infolog: " << infoLog << std::endl;
+	}
 
 	while(!glfwWindowShouldClose(window)) {
 		processInput(window);
