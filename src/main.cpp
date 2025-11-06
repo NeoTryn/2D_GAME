490b5345b252b794b52db4c94c42a372a2dfbbc4
NeoTryn <felixeidler@gmail.com>
2025-11-06 11:36:12 +0100

    First steps

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World!\n";
+
+	glfwWindowHint(GLFW_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	if (!glfwInit()) {
+		std::cout << "GLFW failed to initialize.\n";
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "GAME", nullptr, nullptr);
 }
